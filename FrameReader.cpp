d530455c38cf0bed72be69c86d3dd26afa67838a
Tomasz Jakubczyk <sigrond93@gmail.com>
2016-05-07 14:19:19 +0200

    szkielet klasy czytającej klatki z bufora cyklicznego #2

---
+++ FrameReader.cpp
@@ -0,0 +1,32 @@
+/** \file FrameReader.cpp
+ * \author Tomasz Jakubczyk
+ * \brief Plik z implementacjami metod klasy FrameReader
+ *
+ */
+
+#include "FrameReader.hpp"
+
+FrameReader::FrameReader(CyclicBuffer* c)
+{
+    //ctor
+}
+
+FrameReader::~FrameReader()
+{
+    //dtor
+}
+
+char* FrameReader::pop()
+{
+    return nullptr;
+}
+
+int FrameReader::size()
+{
+    return 0;
+}
+
+void FrameReader::readFrames()
+{
+
+}
